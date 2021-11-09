363b6f780711c74fff2e276b5d334810fa56f13d
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-09 21:56:49 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container');
 let colore;
 let fizzbuzz;
 
- for (let i = 1; i < 101; i++){
+for (let i = 1; i < 101; i++){
     if ((i % 3 == 0) && (i % 5 == 0)) {
          colore = 'red';
          fizzbuzz = 'fizzbuzz';
